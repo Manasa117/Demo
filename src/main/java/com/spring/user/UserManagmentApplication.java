76157cda4bf6235f4a4857f169a9ba99d1949e68
Manasa117 <manasamaani0407@gmail.com>
2023-12-13 13:58:25 +0530

    hlooo

--- src/main/java/com/spring/user/UserManagmentApplication.java
+++ src/main/java/com/spring/user/UserManagmentApplication.java
@@ -8,8 +8,6 @@ public class UserManagmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagmentApplication.class, args);
-
-	//change .1 made by me in my pc
 	}
 
 }
